9717ea004d2be7e595e14aa1637e11f3f562160b
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-17 07:13:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,7 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def self.all_students_in.grade_9
+  def self.all_students_in_grade_9
     sql = <<-SQL  
       SELECT * FROM students WHERE grade = 9
     SQL
