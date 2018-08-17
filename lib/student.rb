408e5f81657285cb1ae88964e4329e75b7c1d933
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-17 07:20:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,9 +85,9 @@ class Student
   
   def self.all_students_in_grade_X(grade)
     sql = <<-SQL 
-      SELEC * FROM students WHERE grade = ?  
+      SELECT * FROM students WHERE grade = ?  
     SQL
-    DB[:conn].execute(sql, grade).map{|row|
+    DB[:conn].execute(sql, grade).each{|row|
       self.new_from_db(row)
     }
   end
